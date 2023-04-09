34c670f3ef9567fb6f21062cabe80ad3477d9b3f
Ruthman Santos <Ruthman Santos@10.0.0.73>
2023-04-08 20:28:48 -0400

    HWProject1

---
+++ src/howework2/Day2HWProject1.java
@@ -0,0 +1,11 @@
+package howework2;
+
+public class Day2HWProject1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Day2HWProject1");
+		System.out.println("Day2HWProject...");
+	}
+
+}
